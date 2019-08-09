2755ee4d3dd14fb25c336d61a5ae8cb5df83e370
Kevin Yang <kyang812@yahoo.com>
2019-08-09 12:59:11 -0700

    drill 2

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,17 +1,57 @@
 
 
-function countOccurences(){
-
+function countOccurences( arr, word ){
+  var count = 0;
+  for ( var val of arr ){
+    if ( val === word){
+      count++;
+    }
+  }
+  return count;
 }
 
-function wordLengths(){
-
+function wordLengths( arr ){
+  var result = [];
+  for ( var val of arr){
+    result.push( val.length );
+  }
+  return result;
 }
 
-function getMinMaxMean(){
-
+function getMinMaxMean( arr ){
+  var min = arr[0]
+  var max = arr[0]
+  var mean = null;
+  for ( var val of arr ){
+    if ( val < min ){
+      min = val;
+    }
+    if ( val > max ){
+      max = val
+    }
+    mean += val
+  }
+  return { min, max, mean: mean/arr.length}
 }
 
-function findMode(){
-
+function findMode( arr ){
+  var obj = {};
+  var highest = {};
+  var highestKey = null;
+  for ( var val of arr ){
+    if ( obj.hasOwnProperty( val ) ){
+      obj[val]+= 1;
+      if ( obj[val] >= highest[val] ){
+        highest = {};
+        highest[val] = obj[val]
+        highestKey = val;
+      } else {
+        highest[val] = obj[val];
+      }
+    } else {
+      obj[val] = 1;
+    }
+  }
+  return highestKey
 }
+
