08d95b740e22bcfc389441760703ace4c01b89fe
Kevin Yang <kyang812@yahoo.com>
2019-08-26 12:49:12 -0700

    drill 3

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -1,19 +1,58 @@
 
 
-function getPath(){
+function getPath(string){
+  return string.split('/');
 
 }
 
-function getPathParts(){
-
+function getPathParts( url ){
+  // var index = url.indexOf('://');
+  // var protocol = url.slice( index);
+  // var host = url.slice( index + 3, url.indexOf(':', index + 1))
+  // var port =
+  // var path;
+  // var file;
+  
+  // url.indexOf('://');
+  
 }
 
-function getCapitalCount(){
-
+function getCapitalCount(arr){
+  let count = 0;
+  for ( var value of arr ){
+    if ( value[0] !== value[0].toLowerCase() ){
+      count++;
+    }
+  }
+  return count;
 }
 
-function correctCalcChecker(){
-
+function correctCalcChecker( obj ){
+  let arrayResult = [];
+  for ( var value of obj){
+    let returnValue;
+    const { num1, num2, op, result } = value;
+    switch ( op ){
+      case '/':
+        returnValue = num1 / num2;
+        break;
+      case '*':
+        returnValue = num1 * num2;
+        break;
+      case '+':
+        returnValue = num1 + num2;
+        break;
+      case '-':
+        returnValue = num1 - num2;
+        break;
+      default:
+        break;
+    }
+    if ( returnValue === result ){
+      arrayResult.push(value );
+    }
+  }
+  return arrayResult;
 }
 
 function doMath(){
